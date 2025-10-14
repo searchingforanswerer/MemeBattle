db039aeed891380d65ac966e46b6c6bce7dab4bc
231220036 <231220036@smail.nju.edu.cn>
2025-10-14 19:38:28 +0800

    添加了一些题目，达到了100道

--- Gitpush.ps1
+++ Gitpush.ps1
@@ -1,10 +1,10 @@
 while ($true) {
     git push origin main
     if ($LASTEXITCODE -eq 0) {
-        Write-Host "✅ Push succeeded!" -ForegroundColor Green
+        Write-Host "[OK] Push succeeded!" -ForegroundColor Green
         break
     } else {
-        Write-Host "❌ Push failed, retrying in 5 seconds..." -ForegroundColor Yellow
+        Write-Host "[Failed] Push failed, retrying in 5 seconds..." -ForegroundColor Yellow
         Start-Sleep -Seconds 5
     }
 }
